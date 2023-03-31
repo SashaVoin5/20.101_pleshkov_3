69e04d52fa8e51d3d79451ec6666b3c3e4fac28c
SashaVoin5 <sasha04ru@mail.ru>
2023-03-31 15:16:34 +0700

    Добавьте файлы проекта.

---
+++ 20.101-pleshkov_3/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using _20._101_pleshkov_3.Entity;
+
+namespace _20._101_pleshkov_3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Search_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                DataGridTextColumn textColumn = new DataGridTextColumn();
+                textColumn.Header = "Номер дисциплины"; //Добавляем столбцы в датагрид
+                textColumn.Binding = new Binding("IdDiscipline");
+                LoadData.Columns.Add(textColumn);
+                DataGridTextColumn textColumn1 = new DataGridTextColumn();
+                textColumn1.Header = "Наименование дисциплины"; 
+                textColumn1.Binding = new Binding("Title");
+                LoadData.Columns.Add(textColumn1);
+                var discipline = Entities.GetContext().Discipline.Where(p => p.IdDiscipline == 2).ToList(); //Считываем из базы данных значения по условию IdDiscipline = 2
+                if (discipline.Count != 0)
+                {
+                    LoadData.ItemsSource = discipline; //Записываем данные в датагрид
+
+                }
+                else
+                {
+                    MessageBox.Show("Ничего не найдено", "Внимание", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                
+
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка", "Внимание", MessageBoxButton.OK,MessageBoxImage.Error);
+            }
+           
+        }
+    }
+}
